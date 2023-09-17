f65b547872b11285982076dd393eefeb0532f445
PedroSilva01 <pedrovictor94442500@gmail.com>
2023-09-16 21:58:53 -0300

    test fix email service

--- js/form.js
+++ js/form.js
@@ -1,7 +1,4 @@
 document.getElementById("form").addEventListener("submit", function(event){
     event.preventDefault();
     document.getElementById("form").style.display = "none";
-    document.getElementById("response-message").innerHTML = "Enviado com sucesso!";
-    // document.getElementById("response-message").style.display = "flex";
-    document.getElementsByClassName("updated-form").style.display = "block";
 })
